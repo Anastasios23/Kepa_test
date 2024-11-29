416a562c5c46b4427e1869da7fdc0ff97eff2808
Anastasios <aandre23@ucy.ac.cy>
2024-11-29 13:18:21 +0200

    Trying to do the signup page

---
+++ register.js
@@ -0,0 +1,72 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+// import {
+//   getDatabase,
+//   set,
+//   ref,
+// } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCyYwtKDV9VZ53RZ22Iy6PEVEhiJdiVoxY",
+  authDomain: "kepa-competition.firebaseapp.com",
+  databaseURL: "https://kepa-competition-default-rtdb.firebaseio.com",
+  projectId: "kepa-competition",
+  storageBucket: "kepa-competition.firebasestorage.app",
+  messagingSenderId: "429461558074",
+  appId: "1:429461558074:web:d5caa1f4c1967dc4abf603",
+  measurementId: "G-3S4J7MH9E7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const database = getDatabase(app);
+// const auth = getAuth();
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+const username = document.getElementById("username").value;
+
+const submit = document.getElementById("submit");
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
+  alert(5);
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  alert(email);
+  alert(password);
+  //const username = document.getElementById("username").value;
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      alert("Creting the user");
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
+    });
+});
+//     signInWithEmailAndPassword(auth, email, password)
+// .then((userCredential) => {
+// // Signed in
+// const user = userCredential.user;
+// set(ref(database), 'users/'+user.uid),{
+// username:username,
+// email:email
+// }
+// alert('User Created')
+// // ...
+// })
+// .catch((error) => {
+// const errorCode = error.code;
+// const errorMessage = error.message;
+// alert(errorMessage);
+// });
+// })
